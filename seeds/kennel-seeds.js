0cf3122c1d5141941c4c53f2937f235d60e1b6c8
Junny <tajo0215@gmail.com>
2021-07-28 13:16:03 -0700

    modified seeding logics to seed the fake data correctly

--- seeds/kennel-seeds.js
+++ seeds/kennel-seeds.js
@@ -3,24 +3,15 @@ const { Kennel } = require("../models");
 const kennelData = [
   {
     name: "Kennel 1",
-    location: "San Diego",
-    isFull: false,
-    pet_id: 2,
-    user_id: 1,
+    capacity: 3,
   },
   {
     name: "Kennel 2",
-    location: "Escondido",
-    isFull: true,
-    pet_id: 1,
-    user_id: 2,
+    capacity: 4,
   },
   {
     name: "Kennel 3",
-    location: "PB",
-    isFull: false,
-    pet_id: 3,
-    user_id: 3,
+    capacity: 2,
   },
 ];
 
