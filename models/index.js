2330d33f15e51ff0511c2494a9d8e4ec1dc78a49
Vivi Cowan <vivicare@gmail.com>
2021-07-28 12:52:17 -0700

    created working associations from proposal

--- models/index.js
+++ models/index.js
@@ -1,39 +1,39 @@
 const User = require("./User");
 const Pet = require("./Pet");
 const Kennel = require("./Kennel");
-const KennelPet = require("./KennelPet");
+const Reservations = require("./Reservations");
 
 // Define sequelize associations in this file.
-// Pet.belongsTo(User, {
-//   foreignKey: "user_id",
-// });
+Pet.hasOne(Reservations, {
+  foreignKey: "pet_id",
+  onDelete: "CASCADE",
+});
 
-// User.hasMany(Pet, {
-//   foreignKey: "user_id",
-//   onDelete: "CASCADE",
-// });
+Reservations.belongsTo(Pet, {
+  foreignKey: "pet_id",
+});
 
-// Kennel.belongsToMany(Pet, {
-//   through: {
-//     model: KennelPet,
-//     unique: true,
-//   },
-// });
-// Kennel.hasMany(Pet, {
-//   foreignKey: "pet_id",
-//   onDelete: "CASCADE",
-// });
+Kennel.hasMany(Reservations, {
+  foreignKey: "kennel_id",
+  onDelete: "CASCADE",
+});
 
-// User.belongsTo(Kennel, {
-//   foreignKey: "pet_id",
-// });
+Reservations.belongsTo(Kennel, {
+  foreignKey: "kennel_id",
+});
 
-// User.belongsToMany(Pet, {
-//   through: {
-//     model: Kennel,
-//     unique: false,
-//   },
-// });
+User.hasMany(Pet, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Pet.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// Pet has may reservations, reservations belong to one pet
+// Kennel has many reservations, reservations have one kennel
+// User has many pets, pet belongs to one user
 
 module.exports = {
   User,
