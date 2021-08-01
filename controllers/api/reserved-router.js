aef12ec9901ba916535de99aafa2cb319f0b2f94
Junny <tajo0215@gmail.com>
2021-08-01 16:22:57 -0700

    reservation removal working correctly

--- controllers/api/reserved-router.js
+++ controllers/api/reserved-router.js
@@ -1,39 +1,11 @@
 const router = require("express").Router();
-const { Pet, Reservations, Kennel } = require("../../models");
-const withAuth = require("../../util/withAuth");
-
-router.get("/", async (req, res) => {
-  try {
-    const reserveData = await Reservations.findAll({
-      include: [
-        {
-          model: Pet,
-        },
-        {
-          model: Kennel,
-          attributes: ["name"],
-        },
-      ],
-    });
-    const reservations = reserveData.map((reservation) =>
-      reservation.get({ plain: true })
-    );
-    // res.render("reservation-list", {
-    //   reservations,
-    //   logged_in: req.session.logged_in,
-    // });
-    res.status(200).json(reservations);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+const { Reservations } = require("../../models");
 
 router.delete("/:id", async (req, res) => {
   try {
     const reservationData = await Reservations.destroy({
       where: {
         id: req.params.id,
-        // user_id: req.session.user_id,
       },
     });
 
